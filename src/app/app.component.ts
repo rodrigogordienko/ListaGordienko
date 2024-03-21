3b7059aa774e4cd684f961d33a11f3e8a66fd613
Rodrigo Gordienko Benítez <rodrigo.gordienko@fing.edu.uy>
2024-03-21 12:17:56 -0300

    Segundo Desafio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ListaGordienko';
+
+  hasError = false;
+  isDisabled = true;
+  students = [{nombre: 'Franco', aprobado: true}, {nombre: 'Isabel', aprobado: true}, {nombre: 'Daniel', aprobado: false}, {nombre: 'Ana', aprobado: false}, {nombre: 'Javier', aprobado: false}, {nombre: 'Melisa', aprobado: true}];
+  mostrarListado = false;
+  status : 'Inscripto' | 'Egresado' | 'ExAlumno' = 'Inscripto';
+  myBirthday = new Date(2000,10,10);
+  fechaPrueba = new Date(2024,6,30);
+  mostrarAprobados = true;
+
+  frutas: string[] = [];
+  frutas2: Array<string> = [];
+
+  alternarError(){
+    this.hasError = ! this.hasError;
+  }
+
+  alternarAprobados(){
+    this.mostrarAprobados = ! this.mostrarAprobados;
+  }
+
+  onSelectChange(ev : Event){
+    console.log("Cambio de estado");
+    const elemento = ev.target as HTMLSelectElement;
+    const valor = elemento.value as
+    | 'ExAlumno'
+    | 'Egresado'
+    | 'Inscripto';
+    this.status = valor; 
+  }
 }
